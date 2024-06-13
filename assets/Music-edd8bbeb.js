9993bb9c77eed0f93ab8aa07d3e3f4a38f4d9cbd
Randomclass12 <1582278023@qq.com>
2024-06-13 23:38:39 +0800

    feat: first commit

---
+++ assets/Music-edd8bbeb.js
@@ -0,0 +1,142 @@
+import{H as o}from"./index-d9871d9d.js";const t=""+new URL("Faded-bce2e25c.mp3",import.meta.url).href,r=""+new URL("TheNewKings-f18139a0.mp3",import.meta.url).href,a=""+new URL("Nevada-af244c06.mp3",import.meta.url).href,i=[{name:"TheNewKings",src:r,lyric:`[00:00.50]Let's dance
+[00:02.10]Cause tonight we're on fire
+[00:04.00]Break out
+[00:06.00]Going straight to the top
+[00:08.00]Let's dance
+[00:09.00]Forever young
+[00:11.00]Here come the new kings
+[00:13.00]Here come the new kings
+[00:15.60]Let's dance
+[00:17.35]Cause tonight we're on fire
+[00:19.45]Break out
+[00:21.08]Going straight to the top
+[00:23.16]Let's dance
+[00:24.96]Forever young
+[00:27.02]Here come the new kings
+[00:28.77]Here come the new kings
+[00:30.47]'Cause this life
+[00:31.59]Coming out as heroes, woah
+[00:36.03]Are we beautiful tonight
+[00:38.95]I've got nothing but love for you
+[00:43.56]'Cause you're beautiful tonight
+[00:58.69]'Cause you're beautiful tonight
+[01:31.26]I don't wanna keep this all to myself
+[01:34.92]I don't wanna stop this feeling
+[01:38.52]I don't wanna keep this all to myself
+[01:42.48]I don't wanna stop this feeling
+[01:45.93]I don't wanna keep this all to myself
+[01:49.81]I don't wanna stop this feeling
+[01:53.59]I don't wanna keep this all to myself
+[01:57.47]I don't wanna stop this feeling
+[02:04.53]Let's dance
+[02:06.09]'Cause tonight we're on fire
+[02:08.14]Break out
+[02:09.85]Going straight to the top
+[02:11.91]Let's dance
+[02:13.74]Forever young
+[02:15.57]Here come the new kings
+[02:17.41]Here come the new kings
+[02:19.31]Let's dance
+[02:21.14]'Cause tonight we're on fire
+[02:23.19]Break out
+[02:24.67]Going straight to the top
+[02:26.89]Let's dance
+[02:28.76]Forever young
+[02:30.58]Here come the new kings
+[02:32.55]Here come the new kings
+[02:35.20]'Cause this life
+[02:35.21]Coming out as heroes, woah
+[02:39.75]Are we beautiful tonight
+[02:42.72]I've got nothing but love for you
+[02:45.39]Woah
+[02:47.27]'Cause you're beautiful tonight
+[02:50.44]What matter as heroes, woah
+[02:54.81]Are we beautiful tonight
+[02:57.86]Tonight tonight tonight tonight
+[03:02.29]'Cause you're beautiful tonight
+`},{name:"Faded",src:t,lyric:`[00:00.000] 作词 : Jesper Borgen/Gunnar Greve Pettersen/Alan Walker/Anders Froen
+[00:01.000] 作曲 : Jesper Borgen/Gunnar Greve Pettersen/Alan Walker/Anders Froen
+[00:10.420]You were the shadow to my light
+[00:13.710]Did you feel us
+[00:17.450]Another start
+[00:19.420]You fade away
+[00:21.380]Afraid our aim is out of sight
+[00:24.260]Wanna see us
+[00:27.700]Alive
+[00:30.590]Where are you now
+[00:35.900]Where are you now
+[00:41.200]Where are you now
+[00:44.040]Was it all in my fantasy
+[00:46.520]Where are you now
+[00:49.260]Were you only imaginary
+[00:53.210]Where are you now
+[00:56.560]Atlantis
+[00:58.520]Under the sea
+[01:01.189]Under the sea
+[01:04.040]Where are you now
+[01:06.439]Another dream
+[01:09.719]The monster's running wild inside of me
+[01:13.920]I'm faded
+[01:19.360]I'm faded
+[01:23.140]So lost, I'm faded
+[01:29.920]I'm faded
+[01:33.900]So lost, I'm faded
+[01:37.230]These shallow waters never met
+[01:40.170]What  I needed
+[01:43.950]I'm letting go
+[01:45.819]A deeper dive
+[01:47.819]Eternal silence of the sea
+[01:50.769]I'm breathing
+[01:54.599]Alive
+[01:57.269]Where are you now
+[02:02.590]Where are you now
+[02:08.099]Under the bright but faded lights
+[02:10.569]You've set my heart on fire
+[02:13.139]Where are you now
+[02:15.830]Where are you now
+[02:18.550]Another dream
+[02:21.939]Another dream
+[02:24.550]Another dream
+[02:27.199]Another dream
+[02:30.629]Where are you now
+[02:33.919]Atlantis
+[02:35.949]Under the sea
+[02:38.530]Under the sea
+[02:41.259]Where are you now
+[02:43.870]Another dream
+[02:47.160]The monster's running wild inside of me
+[02:51.150]I'm faded
+[02:56.620]I'm faded
+[03:00.599]So lost, I'm faded
+[03:07.199]I'm faded
+[03:11.060]So lost, I'm faded
+`},{name:"Nevada",src:a,lyric:`[00:05.01]作曲 : Victor Pool/Ruben den Boer
+[00:06.01]作词 : Cozi Zuehlsdorff/Ruben den Boer/Victor Pool
+[00:19.01]I’ve been painting every fence I know
+[00:22.76]Every color bleeds into the same
+[00:26.64]Cause before you go and walk away
+[00:30.42]Yeah, you'd better know where you’re going
+[00:33.41]Hey ya, hey ya
+[00:38.28]You’re a wanderer just like me
+[00:41.03]Hey ya, hey ya
+[00:46.06]Yeah, you'd better know where you’re going
+[00:52.23]
+[01:20.87]I’ve been painting every fence I know
+[01:25.53]Every color bleeds into the same
+[01:28.86]'Cause before you go and walk away
+[01:32.30]Yeah, you'd better know where you’re going
+[01:35.46]Hey ya, hey ya
+[01:40.19]You’re a wanderer just like me
+[01:43.18]Hey ya, hey ya
+[01:47.89]Yeah, you'd better know where you’re going
+[02:06.44]Hey ya, hey ya
+[02:11.33]You’re a wanderer just like me
+[02:14.07]Hey ya, hey ya
+[02:19.05]Yeah, you'd better know where you’re going
+[02:22.21]
+[02:34.82]Yeah, you'd better know where you’re going
+[02:50.06]Yeah, you'd better know where you’re going
+[02:57.91]You’re a wanderer just like me
+[03:05.67]Yeah, you'd better know where you’re going
+[03:13.32]You’re a wanderer just like me
+`}],s=function(e){return i.find(n=>n.name===e)},u=o("Music",{state:()=>({isPlaying:!1,musicDuration:0,musicName:"Faded",lyric:"",src:"",musicList:["Nevada","Faded","TheNewKings"]}),actions:{getMusicInfo(){let{src:e,lyric:n}=s(this.musicName);this.src=e,this.lyric=n}},getters:{}});export{u as M};
---
+++ assets/Test-e4593760.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-d9871d9d.js";const a={__name:"Test",setup(r){return e(200),(t,s)=>"Test"}};export{a as default};
